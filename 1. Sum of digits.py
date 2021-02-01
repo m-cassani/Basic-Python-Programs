23dcb35b3de31b28b6fdf73471725c60ca0fe903
Marcelo Cassani <60864865+m-cassani@users.noreply.github.com>
2021-02-01 17:30:11 -0300

    Add files via upload

---
+++ 1. Sum of digits.py
@@ -0,0 +1,40 @@
+#############################################################################################
+# This program receives an integer number as input and returns the sum of each digit of it. #
+# Examples: 135 -> 1 + 3 + 5 = 9  or 123456789 = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 = 45     #
+#############################################################################################
+
+sum = 0
+var = 0
+i = 10
+close = False
+
+while (close == False):
+
+    num = int(input("Type an integer number: "))
+
+    if ((num // i) == 0):
+
+        sum = num
+
+    else:
+
+        while ((num // i) > 0):
+
+            var = (num % i)
+
+            if ((var) < 10):
+
+                sum = sum + var
+
+            else:
+                var = ((num % i) - (num % (i/10)))/(i/10)
+                sum = sum + var
+            
+            i = i * 10
+
+        sum = sum + (num // (i/10))
+
+    print("The sum of the digits is:", int(sum), "\n")
+    sum = 0
+    var = 0
+    i = 10
